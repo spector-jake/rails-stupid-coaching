aba77f7b3ed3efa580df32ce5e66bc612f3dd587
Jake Spector <spector.jake@gmail.com>
2019-05-20 13:03:17 +0800

    Added form and response pages

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,19 @@
 class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+    @answer = coach_answer(@question)
+  end
+
+  def coach_answer(your_message)
+    if your_message.capitalize == 'I am going to work right now!'
+      ''
+    elsif your_message.include? '?'
+      'Silly question, get dressed and go to work!'
+    else
+      "I don't care, get dressed and go to work!"
+    end
+  end
 end
--- test/controllers/questions_controller_test.rb
+++ test/controllers/questions_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class QuestionsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get ask" do
+    get questions_ask_url
+    assert_response :success
+  end
+
+  test "should get answer" do
+    get questions_answer_url
+    assert_response :success
+  end
+
 end
